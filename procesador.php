e5c3fef23791c27b035e51a97686760f2b45dc38
Estudiantes <Estudiantes@Labing70208>
2018-04-04 17:56:59 -0500

    version 1

---
+++ procesador.php
@@ -0,0 +1,28 @@
+<?php
+$host="localhost";
+$user="root";
+$password="mysql2018";
+$database="ud";
+$port="3306";
+$conexion=mysqli_connect($host, $user, $password, $database, $port);
+$cadena="SELECT nombre,clave FROM usuario";
+$resultado= mysqli_query($conexion, $cadena);
+
+if(mysqli_num_rows($resultado)>0){
+    while($registro= mysqli_fetch_assoc($resultado)){
+        if($_REQUEST["usuario"]==$registro["nombre"]&&$_REQUEST["clave"]==$registro["clave"]){
+            echo "<br> acceso correcto";
+            echo "usuario: ".$registro ["nombre"]."<br>"."clave: ".$registro ["clave"];
+        }else{
+            echo "<br> acceso denegado";
+        }
+        
+       
+    }
+    
+}else{
+    echo "0 registros";
+}
+
+
+
